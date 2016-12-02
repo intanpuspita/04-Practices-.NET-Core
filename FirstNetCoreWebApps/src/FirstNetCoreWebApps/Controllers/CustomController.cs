7eb63bd083ac8a16d4759f68a8950803f4d92ca3
Intan Puspitasari <intanpusss@gmail.com>
2016-12-02 14:48:43 +0700

    Rename controller + add view & modify layout

---
+++ FirstNetCoreWebApps/src/FirstNetCoreWebApps/Controllers/CustomController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstNetCoreWebApps.Controllers
+{
+    public class CustomController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "This is my welcome action";
+        }
+
+        public string Hello(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, numTimes: {numTimes}");
+        }
+
+        public string HelloID(int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello number: {id}");
+        }
+    }
+}
